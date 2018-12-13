054d4329fd97b00abaa8d93c9768c5cd2002cd03
menno94 <menno-deridder@hotmail.com>
2018-12-13 08:39:03 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='inspectietoolHB',
+      version='0.1',
+      description='...',
+      url='...',
+      author='Menno de ridder',
+      author_email='...',
+      license='MIT',
+      packages=['inspectietoolHB'],
+      zip_safe=False)
